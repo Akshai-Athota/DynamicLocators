6e831587e911e26485af5267c4519c9038cf3d89
Akshai-Athota <akshaiathotaspring001@gmail.com>
2025-05-23 22:18:56 +0530

    find locators method added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,97 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.IOException;
+import java.util.NoSuchElementException;
+
 
 public class App {
-    public static void main(String[] args) throws InterruptedException, IOException {
-        String url ="https://practicetestautomation.com/practice-test-login/";
-        String html = HtmlExtractor.extract(url);
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chand\\IdeaProjects\\DynamicLocators\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        int counter =0;
+        TestScreenRecorder.startRecording();
+        String url = "https://practicetestautomation.com/practice-test-login/";
+        driver.manage().window().maximize();
+        driver.get(url);
+        Thread.sleep(5000);
+        String html = driver.getPageSource();
+
+        System.out.println("sending request to chat gpt");
+
         String locators = LocatorGenarator.genearte(html);
-        System.out.println(locators);
+
+        System.out.println("response received writing to json file");
+
+        LocatorWriter.writeToJson(locators, "loginPage");
+
+        System.out.println("json file completed");
+
+        ElementFinder locatorFinder = new ElementFinder(driver);
+
+        WebElement userNameInput = locatorFinder.findElementParallel(LocatorUtils.getLocators("usernameInput", "loginPage"),
+                30);
+
+        if(userNameInput == null){
+            userNameInput = retry(driver,"usernameInput", "loginPage",counter);
+            counter ++;
+        }
+        userNameInput.sendKeys("student");
+
+        WebElement passwordInput = locatorFinder.findElementParallel(LocatorUtils.getLocators("passwordInput", "loginPage"),
+                30);
+
+        if(passwordInput == null){
+            passwordInput = retry(driver,"passwordInput", "loginPage",counter);
+            counter ++;
+        }
+
+        passwordInput.sendKeys("Password123");
+
+        WebElement submitBtn = locatorFinder.findElementParallel(LocatorUtils.getLocators("submitButton", "loginPage"),
+                30);
+
+        if(submitBtn == null){
+            submitBtn = retry(driver,"submitButton", "loginPage",counter);
+            counter ++;
+        }
+
+        submitBtn.click();
+
+        Thread.sleep(4000);
+
+        TestScreenRecorder.stopRecording();
+
+        driver.quit();
+
+    }
+
+    public static WebElement retry(WebDriver driver,String key,String filePath,int counter) throws Exception {
+        if(counter != 0) throw new NoSuchElementException("None of the locators matched");
+        String html = driver.getPageSource();
+
+        System.out.println("sending request to chat gpt");
+
+        String locators = LocatorGenarator.genearte(html);
+
+        System.out.println("response received writing to json file");
+
+        LocatorWriter.writeToJson(locators, "loginPage");
+
+        System.out.println("json file completed");
+
+        ElementFinder locatorFinder = new ElementFinder(driver);
+
+        WebElement element=locatorFinder.findElementParallel(LocatorUtils.getLocators(key, filePath),
+                30);
+
+        if(element == null){
+            throw new NoSuchElementException("None of the locators matched");
+        }
+        return element;
     }
 }
--- src/main/java/org/example/LocatorGenarator.java
+++ src/main/java/org/example/LocatorGenarator.java
@@ -14,23 +14,22 @@ import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LocatorGenarator {
     public static String genearte(String html) throws IOException {
-        String prompt = "You are an the greatest automation ui tester.For the given page source please provide all the locators in the json format  in the form"
-                + "{locatorName : [list of best 5 locators for the element that i can directly use (xpath|id|class|cssSelectors|partial Link test|names|and so on)]}.\n\n"
-                +"For example : {userName : [//*[@id='username'],input[name='username'],//input[@id='username']]}"
+        String prompt = "You are an the greatest automation ui tester.For the given page source please provide all the locators of the page that can be clickable,interactable,headings,links..etc in the json format  in the form"
+                + "{locatorName : [list of best 5 locators for the web elements that can used to  to locate elements on dom]}.\n\n" +"for input add Input at the end of the locator name and for buttons add Buttons and for links add Link and so on"
+                +"For example : {usernameInput : [xpath=[//*[@id='username'],css=input[name='username'],xpath=//input[@id='username']]}" + "Note : content should be only json no extra information needed\n"
                 + "The given page source is \n\n" + html;
-        String apiKey = "****";
+        String apiKey = "******";
         if (apiKey == null || apiKey.isEmpty()) {
             throw new RuntimeException("OpenAI API key not set in environment variable OPENAI_API_KEY");
         }
 
         JsonObject requestBody = new JsonObject();
-        requestBody.addProperty("model", "gpt-4o-mini"); // or "gpt-4o" or "gpt-4" depending on your access
+        requestBody.addProperty("model", "gpt-4o"); // or "gpt-4o" or "gpt-4" depending on your access
         JsonArray messages = new JsonArray();
 
         // Chat format requires role + content
@@ -58,13 +57,13 @@ public class LocatorGenarator {
             };
 
             String responseBody = client.execute(post, responseHandler);
-            Files.writeString(Paths.get("output.txt"), responseBody, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
 
             JsonObject responseJson = JsonParser.parseString(responseBody).getAsJsonObject();
             JsonArray choices = responseJson.getAsJsonArray("choices");
             if (choices != null && choices.size() > 0) {
                 JsonObject message = choices.get(0).getAsJsonObject().getAsJsonObject("message");
-                return message.get("content").getAsString().trim();
+                String locators = message.get("content").getAsString().trim();
+                return locators.substring(7).split("}")[0]+"}";
             }
             return null;
 
